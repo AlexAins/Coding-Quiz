bb4237d6efe635f766dd2aa32b15766a0c8d7896
Alex Ainslie <alex.ainslie99@gmail.com>
2023-01-14 22:41:21 +0000

    added feedback to answers

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -4,6 +4,7 @@ var startScrEl = document.getElementById("start-screen");
 var questionScrEl = document.getElementById("questions");
 var questionTitleEl = document.getElementById("question-title");
 var questionChoicesEl = document.getElementById("choices");
+var feedbackEl = document.getElementById("feedback");
 
 
 // Setting timer to start when Start button clicked and countdown
@@ -31,25 +32,28 @@ function trial(){
     }
 }
 
-// Button press
+// Answer button press
 questionChoicesEl.addEventListener("click", function(event){
     if(event.target.matches("button")){
         var index = event.target.getAttribute("data-index");
         questionChoicesEl.innerHTML = "";
+        feedbackEl.setAttribute("class","feedback");
 
         if(index == qArrShuffle[qCount].answer){
             qCount++;
             trial();
+            feedbackEl.textContent = "Correct!";
         }
         else{
             qCount++;
             trial();
             timer -= 10;
+            feedbackEl.textContent = "Wrong!";
         }
     }
 })
 
-
+// start button event
 startBntEl.addEventListener("click", function(){
     countdown();
     startScrEl.setAttribute("class", "hide");
