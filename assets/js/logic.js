563198dbd3013ea57c58d355ef47d8d4527b1be7
Alex Ainslie <alex.ainslie99@gmail.com>
2023-01-14 22:36:10 +0000

    created function to move to next question and remove 10 sec if wrong

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -16,22 +16,44 @@ function countdown(){
     }, 1000);
 }
 
-// Function to set first question
-function trial(){
-    questionTitleEl.textContent = qArrShuffle[0].question;
+// Function to set first question - x will become a count for which question.
+qCount = 0;
 
-    for (var i = 0; i < qArrShuffle[0].choices.length ; i++){
+function trial(){
+    questionTitleEl.textContent = qArrShuffle[qCount].question;
+
+    for (var i = 0; i < qArrShuffle[qCount].choices.length ; i++){
 
         var choiceBtn = document.createElement("button");
-        choiceBtn.textContent = i+1 + ": " + qArrShuffle[0].choices[i];
+        choiceBtn.textContent = i+1 + ": " + qArrShuffle[qCount].choices[i];
         questionChoicesEl.appendChild(choiceBtn);
         choiceBtn.setAttribute("data-index", i);
     }
 }
+
+// Button press
+questionChoicesEl.addEventListener("click", function(event){
+    if(event.target.matches("button")){
+        var index = event.target.getAttribute("data-index");
+        questionChoicesEl.innerHTML = "";
+
+        if(index == qArrShuffle[qCount].answer){
+            qCount++;
+            trial();
+        }
+        else{
+            qCount++;
+            trial();
+            timer -= 10;
+        }
+    }
+})
+
 
 startBntEl.addEventListener("click", function(){
     countdown();
     startScrEl.setAttribute("class", "hide");
     questionScrEl.setAttribute("class", "");
     trial();
+    console.log(qArrShuffle);
 });
