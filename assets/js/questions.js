642bc727ef6c9c3214b5f56821894e6c929b2fe0
Alex Ainslie <alex.ainslie99@gmail.com>
2023-01-14 16:54:00 +0000

    Created array of objects with questions and answers

---
+++ assets/js/questions.js
@@ -0,0 +1,14 @@
+// Creating Array of objects containing various questions
+let qArr = [
+{question: "What is the correct HTML tag for creating a Hyperlink?", choices:["<a>Link</a>","<link>Link</link>","<hyper>Link</hyper>","<src>Link</src>"], answer: 0},
+{question: "What does CSS stand for?", choices:["Color and Style Sheets","Creative Style Sheets","Cascading Style Sheets", "None of the Above"], answer: 2},
+{question: "Which of the Following can Javascript Do?", choices:["JavaScript can react to events","JavaScript can manipulate HTML elements","JavaScript can create alerts","All of the Above"], answer: 3},
+{question: "What data type holds values such as `true` and `false`?", choices:["Boolean","String","Number","Undefined"], answer: 0},
+{question: "Which tag is used to make a numbered list?", choices:["<ul>","<ol>","<li>","<nl>"], answer: 1},
+{question: "Which JavaScript method returns the element matching a specified ID?", choices:["getElementsByClass","getElementsById","getElementsByName","getElementById"], answer: 3},
+{question: "What does HTML stand for?", choices:["Hyper Mark Language","Hyper Markup Language","Hyper Mix Language","Hypertension Language"], answer: 1},
+{question: "How do you select an element with a specific class in CSS?", choices:["#","^",".","*"], answer: 2},
+{question: "Which function of an Array removes the last element from the array and returns that element?", choices:["delete()","push()","pop()","remove()"], answer: 2},
+{question: "Which of these is a valid way of representing a color in CSS?", choices:["Valid Color Name","RGB Values","HEX Values","All of the Above"], answer: 3},
+];
+document.getE
