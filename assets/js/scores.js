c56a265b52b9a07b2d046d2c911c9ea101f04c1a
Alex Ainslie <alex.ainslie99@gmail.com>
2023-01-15 22:22:37 +0000

    function to create and clear leaderboard

--- assets/js/scores.js
+++ assets/js/scores.js
@@ -2,8 +2,27 @@
 var clearEl = document.getElementById("clear");
 var highscoresEl = document.getElementById("highscores");
 
+// Making leaderboard from Local Storage
+function LeaderBoard(){
+    if(localStorage.length !== 0 ){
+
+        var hsListFinal = JSON.parse(localStorage.getItem("hs"));
+
+        for (var i = 0; i < hsListFinal.length ; i++){
+            var highScoreList = document.createElement("li");
+            highScoreList.textContent = hsListFinal[i].initials + " - " + hsListFinal[i].score;
+            highscoresEl.appendChild(highScoreList);
+        }
+    }
+    else{
+        highscoresEl.textContent = "Take the quiz to submit a score!"
+    }
+}
+
+LeaderBoard();
 
 // Event listener to clear highscores when button pressed
 clearEl.addEventListener("click", function(){
     localStorage.clear();
+    LeaderBoard();
 })
